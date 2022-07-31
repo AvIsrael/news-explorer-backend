2771cdf410521b223bcf72b7c603147685ec122e
Avihail <stepovoyas@gmail.com>
2022-08-01 01:34:44 +0300

    Final project: stage-1 fixes after 2nd review

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const { requestLogin, errorLogin } = require('./middleware/logging');
 const errorhandler = require('./middleware/errorhandler');
-const NotFoundError = require('./utils/errors/notfounderror');
+const resourceNotFoundError = require('./utils/errors/resourceNotFoundError');
 const { limiter } = require('./utils/ratelimiter');
 const mainRoute = require('./routes/index');
 const { MONGODB_URL } = require('./utils/constants');
@@ -15,16 +15,14 @@ const { PORT = 3000, NODE_ENV, DB_LINK = MONGODB_URL } = process.env;
 const app = express();
 mongoose.connect(DB_LINK);
 
+app.use(requestLogin);
 app.use(limiter);
 app.use(cors());
 app.options('*', cors());
 app.use(express.json());
 app.use(helmet());
-app.use(requestLogin);
 app.use('/', mainRoute);
-app.use('/', (req, res, next) => {
-  next(new NotFoundError('Requested resource not found.'));
-});
+app.use('/', resourceNotFoundError);
 app.use(errorLogin);
 app.use(errors());
 app.use(errorhandler);
