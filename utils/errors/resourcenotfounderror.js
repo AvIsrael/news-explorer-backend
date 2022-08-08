66c5009473ecf8cf2395a62a667c0a9f5967d36a
Avihail <stepovoyas@gmail.com>
2022-08-09 01:49:54 +0300

    Explorer-news: Stage 1 after 1st review v2

--- utils/errors/resourcenotfounderror.js
+++ utils/errors/resourcenotfounderror.js
@@ -1,7 +1,5 @@
 const NotFoundError = require('./notfounderror');
 
-const resourceNotFoundError = (req, res, next) => {
+module.exports.resourceNotFoundError = (req, res, next) => {
   next(new NotFoundError('Requested resource not found.'));
 };
-
-export default resourceNotFoundError;
